623c4d7e9621985c5aed0480e4b6de15ecd903ec
Noel Yap <noel.yap+github.com@gmail.com>
2017-05-02 13:57:03 -0700

    Java version updated.

--- uname/Darwin.sh
+++ uname/Darwin.sh
@@ -1,3 +1,3 @@
 alias ls='ls -AbF'
 
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_31.jdk/Contents/Home
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_131.jdk/Contents/Home
