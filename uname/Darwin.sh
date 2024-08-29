c8002c5775510cee1e6c421db25b47a913a2c719
Noel Yap <noel.yap+github.com@gmail.com>
2024-08-29 15:32:37 -0700

    Homebrew and openjdk added to PATH

--- uname/Darwin.sh
+++ uname/Darwin.sh
@@ -4,4 +4,6 @@ alias ls='ls -AbF --color -D "%Y-%m-%dT%H:%M:%S"'
 
 export BASH_SILENCE_DEPRECATION_WARNING=1
 
-export PATH="${PATH}":/opt/homebrew/bin:/opt/homebrew/sbin
+export CPPFLAGS="-I/opt/homebrew/opt/openjdk@21/include"
+
+export PATH=/opt/homebrew/bin:/opt/homebrew/sbin:/opt/homebrew/opt/openjdk@21/bin:"${PATH}"
