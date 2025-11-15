9b13531ce56bd6c9ecb26d2bd9738692870c41fc
Noel Yap <noel.yap+github.com@gmail.com>
2025-11-15 07:57:55 -0800

    chore(git): switch default branch to main; add LFS filter and advice
    feat(mac): set ANDROID_HOME and JAVA_HOME in Darwin.sh
    
    chore(iTerm2): prune legacy Apple* keys; add findMode_iTerm

--- uname/Darwin.sh
+++ uname/Darwin.sh
@@ -2,8 +2,12 @@ alias cbcopy=pbcopy
 alias ktl=kubectl
 alias ls='ls -AbF --color -D "%Y-%m-%dT%H:%M:%S"'
 
+export ANDROID_HOME="${HOME}/Library/Android/sdk"
+
 export BASH_SILENCE_DEPRECATION_WARNING=1
 
 export CPPFLAGS="-I/opt/homebrew/opt/openjdk@21/include"
+
+export JAVA_HOME="${HOME}/lib/jdk-24.0.2.jdk/Contents/Home"
 
 export PATH=/opt/homebrew/bin:/opt/homebrew/sbin:/opt/homebrew/opt/openjdk@21/bin:"${PATH}"
