6fd71959988e555d3eb95cb2d8d27ad518c261b0
Noel Yap <noel.yap+github.com@gmail.com>
2026-01-15 12:58:53 -0800

    idea function created

--- uname/Darwin.sh
+++ uname/Darwin.sh
@@ -2,6 +2,11 @@ alias cbcopy=pbcopy
 alias ktl=kubectl
 alias ls='ls -AbF --color -D "%Y-%m-%dT%H:%M:%S"'
 
+idea() {
+  open -na '/Applications/IntelliJ IDEA.app' --args "$@"
+}
+export -f idea
+
 export ANDROID_HOME="${HOME}/Library/Android/sdk"
 
 export BASH_SILENCE_DEPRECATION_WARNING=1
